16c0c95956b7551d1325862a252b6401efb9ccce
Ji_Subin <qls1178@naver.com>
2024-12-26 15:18:20 +0900

    Initial commit

---
+++ SpartaProject2/Animal.h
@@ -0,0 +1,85 @@
+// #pragma once
+#ifndef ANIMAL_H
+#define ANIMAL_H
+
+#include<iostream>
+using namespace std;
+
+class Animal {
+public:
+	virtual ~Animal(){}
+	virtual void makeSound() = 0;
+};
+
+class Dog : public Animal {
+public:
+	~Dog() {
+		cout << "Dog out\n";
+	}
+	void makeSound()
+	{
+		cout << "Woof!\n";
+	}
+};
+
+class Cat : public Animal {
+	~Cat() {
+		cout << "Cat out\n";
+	}
+	void makeSound()
+	{
+		cout << "Meow!\n";
+	}
+};
+
+class Cow : public Animal {
+	~Cow() {
+		cout << "Cow out\n";
+	}
+	void makeSound()
+	{
+		cout << "Moo!\n";
+	}
+};
+
+class Zoo {
+private:
+	Animal** animals;
+	int size;		// 동물원크기
+	int count;		// 동물수			
+public:
+	Zoo(int size) : size(size), count(0) {
+		animals = new Animal * [size];
+	}
+	~Zoo() {
+		cout << "==================================================\n";
+
+		for (int i = 0; i < count; i++) {
+			if (animals[i] != nullptr) {
+				delete animals[i];
+			}
+		}
+
+		cout << "==================================================\n";
+		cout << "동물원 닫음\n";
+
+		delete animals;
+	}	
+	void addAnimal(Animal* animal) {
+		if (size < count + 1) {			
+			cout << "동물원이 가득찼습니다.\n";
+		}
+		else {
+			animals[count] = animal;
+			count++;
+			cout << "동물원 크기: " << size << " 현재 동물 수: " << count << "\n";
+		}
+	}
+	void performActions() {
+		for (int i = 0; i < count; i++) {
+			animals[i]->makeSound();
+		}
+	};
+};
+
+#endif 
---
+++ SpartaProject2/Main.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <random>
+#include <crtdbg.h>		// 메모리누수확인
+#include "Animal.h"
+
+#if _DEBUG
+#define new new(_NORMAL_BLOCK,__FILE__,__LINE__)
+#define malloc(s) _malloc_dbg(s,_NORMAL_BLOCK,__FILE__,__LINE__)
+#endif
+
+using namespace std;
+
+Animal* createRandomAnimal() {
+	random_device rd;
+	mt19937 generator(rd());
+	uniform_int_distribution<int> distribution(0, 2);
+
+	int index = distribution(generator);
+	
+	Animal* animal;
+
+	// Dog : 0 / Cat : 1 / Cow : 2
+	switch (index)
+	{
+	case 1:
+		animal = new Cat();
+		break;
+	case 2:
+		animal = new Cow();
+		break;
+	default:
+		animal = new Dog();
+		break;
+	}
+
+	return animal;
+}
+
+int main()
+{
+	int zooAnimalCount = 10; // 동물수
+	int zooSize = 10; // 동물원 크기
+
+	{
+		Zoo zoo(zooSize);
+		while (zooAnimalCount != 0) {
+			zoo.addAnimal(createRandomAnimal());
+			zooAnimalCount--;
+		}
+
+		cout << "==================================================\n";
+
+		zoo.performActions();
+	}
+
+	-_CrtDumpMemoryLeaks();
+	return 0;
+}
